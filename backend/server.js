8b54aca1219992175dd35754b679b1a33cde005f
Kenneth Aladi <kennethmichael11@gmail.com>
2019-09-30 18:40:22 +0100

    Created server and app for backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const http = require('http');
+const http = require("http");
+const app = require("./app");
 
+app.set("port", process.env.PORT || 3000);
+const server = http.createServer(app);
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-    res.end('This is my Server response!');
-});
+//     res.end('This is my first Server response!');
+// });
 
 server.listen(process.env.PORT || 3000);
