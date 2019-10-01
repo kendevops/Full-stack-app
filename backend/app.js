153d4a4bc39744cacbff1c233b83c15b8ec2ecd1
Kenneth Aladi <kennethmichael11@gmail.com>
2019-10-01 18:46:40 +0100

    Working on the backend of the app

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
-module.export = app;
+app.use((req, res) => {
+  res.json({ message: "Your request was successful" });
+});
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const app = require("./app");
 
 app.set("port", process.env.PORT || 3000);
+
 const server = http.createServer(app);
 
 // const server = http.createServer((req, res) => {
