0ef534a58f14f76e0bd209f75d07eb677f8df3fd
Kenneth Aladi <kennethmichael11@gmail.com>
2019-11-08 05:41:31 +0100

    Created Authentication and bcrypt for the email and password

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -37,5 +38,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
